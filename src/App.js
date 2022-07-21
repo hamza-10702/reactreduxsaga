f0a273f91207a316cef01e74ab6da90602630710
MZ <iammurtazaali53@gmail.com>
2022-07-21 10:12:06 +0500

    Action Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { addToCart } from "./redux/action";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const product = {
+    itemId: 1,
+    itemName: "Mobile",
+    itemPrice: 23000,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => dispatch(addToCart(product))}
+      >
+        Add To Cart
+      </button>
     </div>
   );
 }
---
+++ src/redux/action.js
@@ -0,0 +1,8 @@
+export const addToCart = (dataPayload) =>{
+    console.log("This is action  which is takes data from react component and send to reducer by using type");
+    console.warn(dataPayload);
+    return{
+        type: "ADD_TO_CART",
+        dataPayload,
+    }
+}
