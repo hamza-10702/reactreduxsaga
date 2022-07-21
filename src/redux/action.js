fef71adbda08d8ba1394f4ad2491039420a54ce3
MZ <iammurtazaali53@gmail.com>
2022-07-21 11:25:33 +0500

    new action created remove to cart and change reducer

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,9 +1,19 @@
-import { ADD_TO_CART } from "./constant";
+import { ADD_TO_CART , REMOVE_TO_CART } from "./constant";
 export const addToCart = (dataPayload) =>{
     console.log("This is action  which is takes data from react component and send to reducer by using type");
     console.warn(dataPayload);
     return{
         type: ADD_TO_CART,
         dataPayload,
+    }
+}
+let localData = "local data";
+
+export const removeToCart = () =>{
+    console.log("This is action  which is takes data from react component and send to reducer by using type");
+    console.log("This is remove to cart data ")
+    return{
+        type: REMOVE_TO_CART,
+        localData
     }
 }
